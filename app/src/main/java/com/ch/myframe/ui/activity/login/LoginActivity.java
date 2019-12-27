1eea4a0b580c12d940aa14c23ce0d3e4b02fddb1
love197ch <1160101149@qq.com>
2019-12-27 14:17:38 +0800

    1.新增AndroidUtilCode工具类库适配

--- app/src/main/java/com/ch/myframe/ui/activity/login/LoginActivity.java
+++ app/src/main/java/com/ch/myframe/ui/activity/login/LoginActivity.java
@@ -4,12 +4,14 @@ import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.blankj.utilcode.util.TimeUtils;
 import com.ch.myframe.R;
 import com.ch.myframe.base.BaseMvpActivity;
 import com.ch.myframe.bean.User;
